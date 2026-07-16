e41150569f34b5db1977b008b65440ba10c0d016
Soham Sangole <sohamajaysangole@gmail.com>
2026-07-16 07:00:16 +0530

    Rename package from pygraphit to pygraph-mapper in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="pygraphit",
+    name="pygraph-mapper",
     version="0.1.0",
     author="Soham Sangole",
     author_email="sasangole2019@gmail.com",
